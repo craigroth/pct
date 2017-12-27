99f7594c5f30198d9eec0a963312bd9f1b393730
craigroth <craigroth@users.noreply.github.com>
2017-12-27 13:34:34 -0500

    modified import of miceadds
    changed importing method to use the DESCRIPTION file.

--- R/pct.R
+++ R/pct.R
@@ -1,11 +1,10 @@
-require(miceadds)
+
 
 # convert number to percent as character variable
-# input: 
+# input:
   # numbertobeconvertedtocharacter = numeric value
   # numberofdecimals = number of decimal places in percentage (default: 0)
 # output: character with that number written as a percent
 pct <- function(numbertobeconvertedtocharacter, numberofdecimals = 0){
-  require(miceadds)
-  return(paste0(round2(numbertobeconvertedtocharacter*100,numberofdecimals),"%"))
+  return(paste0(miceadds::round2(numbertobeconvertedtocharacter*100,numberofdecimals),"%"))
 }
